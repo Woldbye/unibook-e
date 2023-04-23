888b7909f53bc464165d4e23301733f18db5f906
Christoffer Woldbye <woldby3@hotmail.com>
2023-04-23 20:41:52 +0200

    * Added router and a recurring topbar for all pages

---
+++ client/src/pages/Book.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import {
+  Container,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
+
+
+const Book = () => (
+  <Container>
+    <Stack>
+      <Text>BOOKING</Text>
+    </Stack>
+  </Container>
+)
+
+export default Book;
