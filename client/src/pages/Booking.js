fadd1cab39083e2660b850d633aca5e7a8d9c107
Christoffer Woldbye <woldby3@hotmail.com>
2023-05-09 22:32:58 +0200

    Added minimum value to GoggleInput for more flexibility

--- client/src/pages/Booking.js
+++ client/src/pages/Booking.js
@@ -14,8 +14,8 @@ const Booking = () => (
     <VStack paddingBottom='2rem'>
       <Text color={Color.BLACK} fontSize={'3xl'} padding={'30px 0px 10px 0px'}>Vælg lokale krav</Text>
       <VStack width='40%' spacing={'1rem'} minWidth={'12rem'}>
-        <GoggleInput step_per_click={1} type_name={'Personer'} start_value={1} max_value={128} />
-        <GoggleInput step_per_click={0.5} type_name={'Timer'} start_value={2.0} max_value={14.0} /> 
+        <GoggleInput step_per_click={1} type_name={'Personer'} start={1} max={128} min={1} />
+        <GoggleInput step_per_click={0.5} type_name={'Timer'} start={2.0} max={14.0} min={0.5} /> 
       </VStack>
       <Container padding={"3rem 0 0 0"} centerContent="true">
         <Background width="60vw" height="40vh" backgroundColor={Color.BLUE}>
