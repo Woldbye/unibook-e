0c1ac84161cbd51bdedcacc61eb66e93bddb6055
Anna <annaj@itu.dk>
2023-05-26 21:44:22 +0200

    Styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -11,7 +11,7 @@ import { Link } from 'react-router-dom';
 
 const Home = () => (
     <Container padding={"3rem 0 7rem 0"} centerContent="true">
-      <Background width="80vw" height="125vh" backgroundColor={Color.BLUE}>
+      <Background width="80vw" height="auto" backgroundColor={Color.BLUE}>
         <VStack>
           <Text color={Color.CREME} fontSize={'4xl'} paddingTop={'50px'}>Velkommen til</Text>
           <Text color={Color.CREME} fontSize={'4xl'} paddingBottom={'50px'}>UNIBOOK-E</Text>
@@ -26,7 +26,7 @@ const Home = () => (
           <Text color={Color.CREME} padding={'0px 40px 0px 40px'} textAlign="center">UNIBOOK-E er en hjemmeside til ITU. Det er en side hvor du kan se
            og booke lokaler på ITU. Hjemmesiden er udviklet af 4 studerende på ITU i faget UX & Webprogrammering.</Text>
           <Text color={Color.CREME} fontSize={'xl'} padding={'20px 40px 0px 40px'} textAlign="center">Kontakt os</Text>
-          <Text color={Color.CREME} padding={'0px 40px 0px 40px'} textAlign="center">For hurtig hjælp ring på Tlf :  ** ** ** ** mellem 8:30 og 13:30 
+          <Text color={Color.CREME} padding={'0px 40px 50px 40px'} textAlign="center">For hurtig hjælp ring på Tlf :  ** ** ** ** mellem 8:30 og 13:30 
           mandag til torsdag. Hvis ikke det er muligt, kan vi kontaktes på emailSomething@gmail.com. Svartid variere.</Text>
         </VStack>
       </Background>
